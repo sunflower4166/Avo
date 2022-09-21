5b144c3cd448a967e83356e307d5342ef5acd89c
UnderCoverSunFlower <84867567+sunflower4166@users.noreply.github.com>
2022-09-21 15:27:38 -0400

    Update avo.js

--- avo.js
+++ avo.js
@@ -139,7 +139,7 @@ thestyle.innerText += ".avoclosebtn:hover {background: #1c1c1c;}"
 // .active
 thestyle.innerText += ".avoactive {background: blue; color: white;}"
 
-thestyle.innerText += ".avoactive:hover {background: orange;}"
+thestyle.innerText += ".avoactive:hover {background: blue;}"
 
 // #loginput
 thestyle.innerText += "#avologinput {outline: none; border: none; width: 100%; font-size: 15px;background:black;color:white;}"
